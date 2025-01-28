ac9fa7028b364d8652322dbab58f5ee2a5948f4f
dev3.bisa <dev3.bisa@gmail.com>
2025-01-27 23:51:55 -0300

    feat: add TMDB service for main communication with external API

---
+++ app/Services/TMDBService.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+class TMDBService{
+    protected $baseUrl;
+    protected $apiKey;
+
+    public function __construct(){
+        $this->baseUrl = "https://api.themoviedb.org/3";
+        $this->apiKey = env('TMDB_API_KEY');
+    }
+
+    private function getHeaders($headerData = []){
+        if (!empty($headerData)){
+            return array_merge($headerData, [
+                'api_key' => $this->apiKey,
+                'Accept' => 'application/json',
+            ]);
+        }
+        return [
+            'api_key' => $this->apiKey,
+            'Accept' => 'application/json',
+        ];
+    }
+
+    public function getPopularSeries()
+    {
+        $response = Http::get("{$this->baseUrl}/tv/popular", $this->getHeaders());
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+        $message = $response->json()['status_message'] ?? 'Unknown error';
+        $status = $response->status();
+
+        throw new Exception($message, $status);
+    }
+
+    public function getSerieById($serieId)
+    {
+        $response = Http::get("{$this->baseUrl}/tv/{$serieId}", $this->getHeaders());
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        $message = $response->json()['status_message'] ?? 'Unknown error';
+        $status = $response->status();
+
+        throw new Exception($message, $status);
+    }
+
+    public function searchSeriesByName($query)
+    {
+        $headerData = ['query' => $query];
+        $response = Http::get("{$this->baseUrl}/search/tv", $this->getHeaders($headerData));
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        $message = $response->json()['status_message'] ?? 'Unknown error';
+        $status = $response->status();
+
+        throw new Exception($message, $status);
+
+    }
+
+    public function getAllGenres()
+    {
+        $response = Http::get("{$this->baseUrl}/genre/tv/list", $this->getHeaders());
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        $message = $response->json()['status_message'] ?? 'Unknown error';
+        $status = $response->status();
+
+        throw new Exception($message, $status);
+
+    }
+
+    public function searchSeriesByGenre($genre)
+    {
+        $headerData = ['with_genres' => $genre];
+        $response = Http::get("{$this->baseUrl}/discover/tv", $this->getHeaders($headerData));
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        $message = $response->json()['status_message'] ?? 'Unknown error';
+        $status = $response->status();
+        throw new Exception($message, $status);
+    }
+
+}
