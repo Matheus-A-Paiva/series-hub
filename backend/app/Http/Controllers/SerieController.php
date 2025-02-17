d71387927c500f61d9633de9c355aefd3d225d52
dev3.bisa <dev3.bisa@gmail.com>
2025-02-16 21:05:47 -0300

    feat: add Redis caching to optimize series endpoints

--- backend/app/Http/Controllers/SerieController.php
+++ backend/app/Http/Controllers/SerieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\TMDBService;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class SerieController extends Controller
 {
@@ -19,7 +20,16 @@ class SerieController extends Controller
     {
         try{
             $page = $request->query('page', 1);
+            $cacheKey = "series_page_{$page}";
+
+            if(Redis::exists($cacheKey)){
+                return response()->json([json_decode(Redis::get($cacheKey))], 200);
+            }
+
             $topRatedSeries = $this->tmdbService->getTopRatedSeries($page);
+
+            Redis::setex($cacheKey, 600, json_encode($topRatedSeries));
+
             return response()->json([$topRatedSeries], 200);
         } catch (Exception $e) {
             return response()->json([
@@ -32,7 +42,13 @@ class SerieController extends Controller
     public function show($serieId)
     {
         try{
+            $cacheKey = "series_show_{$serieId}";
+            if(Redis::get($cacheKey)){
+                return response()->json([json_decode(Redis::get($cacheKey))], 200);
+            }
             $serieDetail = $this->tmdbService->getSerieById($serieId);
+
+            Redis::setex($cacheKey, 600, json_encode($serieDetail));
 
             return response()->json($serieDetail, 200);
         } catch (Exception $e) {
@@ -47,8 +63,18 @@ class SerieController extends Controller
 
         try{
             $page = $request->query('page', 1);
+
             $genres = $request->query('with_genres');
+            $cacheKey = "series_genre_{$genres}_page_{$page}";
+
+            if(Redis::exists($cacheKey)){
+                return response()->json([json_decode(Redis::get($cacheKey))], 200);
+            }
+
             $response = $this->tmdbService->searchSeriesByGenre($genres, $page);
+
+            Redis::setex($cacheKey, 600, json_encode($response));
+
             return response()->json([$response], 200);
         } catch (Exception $e) {
             return response()->json([$e->getMessage()], $e->getCode());
@@ -61,7 +87,14 @@ class SerieController extends Controller
         try{
             $page = $request->query('page', 1);
             $query = $request->query('query');
+            $cacheKey = "series_query_{$query}_page_{$page}";
+
+            if(Redis::exists($cacheKey)){
+                return response()->json([json_decode(Redis::get($cacheKey))], 200);
+            }
             $response = $this->tmdbService->searchSeriesByName($query, $page);
+
+            Redis::setex($cacheKey, 600, json_encode($response));
 
             return response()->json([$response], 200);
         } catch (Exception $e) {
