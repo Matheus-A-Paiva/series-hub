b58f3335b083c94bd589230282e29648210e1dbd
dev3.bisa <dev3.bisa@gmail.com>
2025-02-16 21:36:52 -0300

    fix: ensure Redis uses TLS configuration in database settings

--- backend/config/database.php
+++ backend/config/database.php
@@ -151,21 +151,23 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
+            'scheme' => env('REDIS_SCHEME', 'tls'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'port' => env('REDIS_PORT', 6379),
+            'password' => env('REDIS_PASSWORD', null),
+            'database' => env('REDIS_DB', 0),
+            'read_timeout' => 60,
+            'timeout' => 60,
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
+            'scheme' => env('REDIS_SCHEME', 'tls'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => env('REDIS_PORT', 6379),
+            'password' => env('REDIS_PASSWORD', null),
+            'database' => env('REDIS_CACHE_DB', 1),
+            'read_timeout' => 60,
+            'timeout' => 60,
         ],
 
     ],
